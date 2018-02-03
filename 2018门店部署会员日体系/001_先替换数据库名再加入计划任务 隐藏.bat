1e7db0f1bca043c8dfab3af1b025c3a7e6a90785
zgxx <dismark3@163.com>
2018-02-03 17:05:49 +0800

    部署

--- 2018门店部署会员日体系/001_先替换数据库名再加入计划任务 隐藏.bat
+++ 2018门店部署会员日体系/001_先替换数据库名再加入计划任务 隐藏.bat
@@ -1,4 +1,9 @@
 echo 开始更新会员日明细,请勿关闭
-@echo off
+@echo off 
+if "%1"=="h" goto begin 
+
+start mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit 
+
+:begin 
 osql -S 127.0.0.1 -d qwe -U sa -P Hx789789 -i D:\成都海翔软件有限公司\2018门店部署会员日体系\00_B_新版本按处方药类别促销插入范例_插入明细部分.sql
 exit
